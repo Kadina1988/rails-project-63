ae5669e072353dcfc64198d346858e4925aa7c90
Kadina1988 <kadina.aziev@mail.ru>
2023-10-07 10:28:03 +0300

    changed method build

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,11 +1,13 @@
 require 'erb'
 
 module HexletCode::Tag
-  def self.build(tag)
-    template = "<#{tag}>"
+  def self.build(*tag)
+    string = tag.join(' ')
+    arr = string.chars
+    arr.delete_if { |c| c.start_with?('{', '}', ':', '>', ',') }
+    res = arr.join
+    template = "<#{res}>"
     renderer = ERB.new(template)
-    renderer.result(binding)
+    puts renderer.result(binding)
   end
 end
-
-
