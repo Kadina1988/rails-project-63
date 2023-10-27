50ee4baea93cd72851806484fbe4bd0015189c74
Kadina1988 <kadina.aziev@mail.ru>
2023-10-27 09:31:14 +0300

    step 5

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -9,16 +9,34 @@ module HexletCode
   def self.form_for(obj, path = nil)
     yield obj
 
-    template = if path.nil?
+    template =  if path.nil?
                  "<form action='#' method='post'></form>"
-               else
+                else
                  "<form action='#{path[:url]}' method='post'></form>"
-               end
+                end
 
     renderer = ERB.new(template)
     renderer.result(binding)
   end
 
+
+
   class Error < StandardError; end
   # Your code goes here...
 end
+
+
+class A
+  def self.my_method(attribute)
+    define_method attribute do
+      self.instance_variable_get "@#{attribute}"
+    end
+  end
+
+  my_method :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,8 +3,11 @@
 require_relative 'test_helper'
 
 class TestHexletCode < Minitest::Test
-  User = Struct.new(:name)
-  USER = User.new(name: 'rob')
+  User = Struct.new(:name, keyword_init: true)
+
+  def setup
+    @user = User.new
+  end
 
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
@@ -17,15 +20,44 @@ class TestHexletCode < Minitest::Test
   def test_form_for_without_path
     expect = "<form action='#' method='post'></form>"
 
-    actual = HexletCode.form_for USER do |f|
+    actual = HexletCode.form_for @user do |f|
     end
+
     assert_equal(expect, actual)
   end
 
   def test_form_for_with_path
     expect = "<form action='/user' method='post'></form>"
 
-    actual = HexletCode.form_for USER, url: '/user' do |f|
+    actual = HexletCode.form_for @user, url: '/user' do |f|
+    end
+
+    assert_equal(expect, actual)
+  end
+
+  def test_form_for_with_additional_parametres
+    user = User.new name: 'Rob'
+
+    expect = "<form action='#' method='post'
+               input name='name' type='text' value='rob' class='user-input'
+               input name='job' type='text' value='hexlet'>
+             </form>"
+
+    actual = HexletCode.form_for user, url: '#' do |f|
+      f.input :name
+      f.input :job
+    end
+
+    assert_equal(expect, actual)
+  end
+
+  def test_form_for_with_defolt_value
+    expect = "<form action='#' method='post'>
+               <textarea name='job' cols='20' rows='40'>hexlet</textarea>
+             </form>"
+
+    actual = HexletCode.form_for @user do |f|
+               f.input :job, as: :text
     end
 
     assert_equal(expect, actual)
