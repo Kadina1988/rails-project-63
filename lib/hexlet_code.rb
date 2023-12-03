09c5f86ebfc47f4e1676879f9995a2da79e31fff
Kadina1988 <kadina.aziev@mail.ru>
2023-12-03 18:20:54 +0300

    fix hexlet_code.rb

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,9 +3,9 @@
 require_relative 'hexlet_code/version'
 
 module HexletCode
-  autoload(:Tag, './lib/hexlet_code/tag')
-  autoload(:Form, './lib/hexlet_code/form')
-  autoload(:Renderer, './lib/hexlet_code/renderer')
+  autoload(:Tag, File.join(File.dirname(FILE), './lib/hexlet_code/tag'))
+  autoload(:Form, File.join(File.dirname(FILE), './lib/hexlet_code/form'))
+  autoload(:Renderer, File.join(File.dirname(FILE), './lib/hexlet_code/renderer'))
 
   def self.form_for(obj, params = {})
     form = Form.new(obj)
