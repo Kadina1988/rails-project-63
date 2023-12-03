c53a95a3b0e122de0d73766c9844c5fddfe749bd
Kadina1988 <kadina.aziev@mail.ru>
2023-12-03 22:05:22 +0300

    f

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'hexlet_code/version'
+require_relative 'hexlet_code/tag'
+require_relative 'hexlet_code/form'
+require_relative 'hexlet_code/renderer'
 
 module HexletCode
   autoload(:Tag, './lib/hexlet_code/tag')
